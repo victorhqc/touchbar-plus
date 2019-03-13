888d6fae5dbff9b411613ba1dda84cbe8df56523
Victor Quiroz Castro <victorhqc@gmail.com>
2019-03-13 22:38:04 +0100

    Update renderer

--- src/touchbar/start.js
+++ src/touchbar/start.js
@@ -3,12 +3,13 @@
 /* eslint import/no-extraneous-dependencies: 0 */
 
 import React from 'react';
+import { TouchBar as NativeTouchBar } from 'remote';
 import { ReactTouchBar, TouchBar } from 'touchbar-electron-renderer';
 import Root from './Root';
 
 export default function start(history) {
   console.time('USING-REACT-RENDERER');
-  const result = ReactTouchBar.render(<Root history={history} />, new TouchBar(atom.getCurrentWindow()));
+  const result = ReactTouchBar.render(<Root history={history} />, new TouchBar(atom.getCurrentWindow(), NativeTouchBar));
   console.timeEnd('USING-REACT-RENDERER');
   return result;
 }

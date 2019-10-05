9471868d5a142206b122a78a1929f177d66a34bf
Victor Quiroz Castro <victorhqc@gmail.com>
2019-10-05 14:41:43 +0200

    fix: build & compile

--- src/touchbar/App.tsx
+++ src/touchbar/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 // import { Switch, Route } from 'react-router';
 import RendererDemo from './RendererDemo';
+// import DemoReel from './DemoReel';
 
 // import TextEditor from './views/TextEditor';
 // import TreeView from './views/TreeView';
--- src/touchbar/start.tsx
+++ src/touchbar/start.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserWindow, TouchBar as NativeTouchBar } from 'electron';
+import { BrowserWindow, remote } from 'electron';
 import { History } from 'history';
 import { ReactTouchBar, TouchBar } from 'touchbar-electron-renderer';
 import Root from './Root';
+const { TouchBar: NativeTouchBar } = remote;
 
 export default function start(history: History) {
   console.time('USING-REACT-RENDERER');
   const atomWindow = atom.getCurrentWindow() as BrowserWindow;
 
-  const result = ReactTouchBar.render(
+  ReactTouchBar.render(
     <Root history={history} />,
     new TouchBar(atomWindow, NativeTouchBar),
     () => {},
   );
   console.timeEnd('USING-REACT-RENDERER');
-  return result;
 }

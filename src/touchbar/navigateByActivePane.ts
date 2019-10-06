1477c691fad9e80a78ff8ff3f0abf48f7dc0e888
Victor Quiroz Castro <victorhqc@gmail.com>
2019-10-06 20:30:53 +0200

    feat: improved navigation with heuristics
    - unknown route
    - route when pane is a proxy

--- src/touchbar/navigateByActivePane.ts
+++ src/touchbar/navigateByActivePane.ts
@@ -3,15 +3,47 @@ import { logger } from '../utils';
 
 let activePane: string | null = null;
 
-function navigateByActivePane(history: History, item: Item | null) {
-  // Check if it makes sense to update TouchBar
-  if (!item || !item.constructor || activePane === item.constructor.name) {
-    return;
-  }
-  activePane = item.constructor.name;
+function navigateByActivePane(history: History, item: ItemPane | null) {
+  const navigate = navigateTo(history);
 
-  logger.debug(`Navigate to route /${activePane.toLowerCase()}`);
-  history.push(`/${activePane.toLowerCase()}`);
+  if (!item) {
+    return navigate('__unknown__');
+  }
+
+  if (item.hasOwnProperty('getElement') && item.getElement) {
+    const element = item.getElement();
+    navigateWithElement(history, element);
+  } else if (item.constructor && item.constructor.name) {
+    navigate(item.constructor.name.toLowerCase());
+  } else {
+    navigate('__unknown__');
+  }
+}
+
+function navigateTo(history: History) {
+  return (routeName: string) => {
+    if (activePane === routeName) {
+      return;
+    }
+
+    activePane = routeName;
+
+    logger.debug(`Navigate to route /${routeName}`);
+    history.push(`/${routeName}`);
+  };
+}
+
+/**
+ * Navigates to some special panes. This is a naive implementation that most likely is going to
+ * break but is good for now.
+ */
+function navigateWithElement(history: History, element: HTMLElement) {
+  const classList = element.classList;
+  logger.debug(`classList, ${JSON.stringify(classList)}`);
+
+  if (classList.contains('github-Git-root')) {
+    navigateTo(history)('github-pane');
+  }
 }
 
 export default navigateByActivePane;
@@ -20,4 +52,7 @@ export interface Item {
   constructor: {
     name: string;
   };
+  getElement?: () => HTMLElement;
 }
+
+type ItemPane = Item & object;

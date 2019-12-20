8de44e716fcdd6c344041f78729c8aef8b9f2a6f
Victor Quiroz Castro <victorhqc@gmail.com>
2019-12-20 21:44:04 +0100

    build: postinstall message

--- postinstall.sh
+++ postinstall.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 if [[ -z "$REBUILD" ]]; then
-  echo "Post install disabled"
+  echo "Post install disabled, if you want to rebuild native dependencies, run the following"
+  echo "REBUILD=1 npm i"
 else
   npm run rebuild
 fi
